e3cf868bd2a96722ea4e383708da93e6571b0981
liuguiyangnwpu <liuguiyangnwpu@163.com>
2015-12-15 19:00:59 +0800

    New Scrapy Proj

---
+++ News_Scrapy/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for News_Scrapy project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'News_Scrapy'
+
+SPIDER_MODULES = ['News_Scrapy.spiders']
+NEWSPIDER_MODULE = 'News_Scrapy.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'News_Scrapy (+http://www.yourdomain.com)'
