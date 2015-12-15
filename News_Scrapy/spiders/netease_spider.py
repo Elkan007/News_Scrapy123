dc63927a8a57e271e65576e5d1ece6beffaeabfb
liuguiyangnwpu <liuguiyangnwpu@163.com>
2015-12-15 23:24:33 +0800

    Modified the Content

--- News_Scrapy/spiders/netease_spider.py
+++ News_Scrapy/spiders/netease_spider.py
@@ -18,11 +18,12 @@ class NetEaseSpider(CrawlSpider):
     def parse_item(self,response):
         sel_resp = Selector(response)
         news_item = NewsScrapyItem()
-        news_item["news_type"] = sel_resp.xpath('/html/body/div[4]/div[3]/div/span[2]/a[2]/text()').extract()
+        tmp = response.url.split("/")[2]
+        news_item["news_type"] = tmp.split(".")[0]
         news_item["news_title"] = sel_resp.xpath('//*[@id="h1title"]/text()').extract()
         news_item["news_date"] = sel_resp.xpath('//*[@id="epContentLeft"]/div[1]/div[1]/text()').re(r'[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}')
         news_item["news_source"] = sel_resp.xpath('//*[@id="ne_article_source"]/text()').extract()
-        news_item["news_content"] = sel_resp.xpath('//*[@id="endText"]/text()').extract()
+        news_item["news_content"] = sel_resp.xpath('//*[@id="endText"]').extract()
         return news_item
 
 
